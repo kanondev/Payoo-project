498f4d8c37bce8cac7926a53c62c932698259611
kanondev <kanon2017sengrem@gmail.com>
2024-09-19 14:34:12 +0600

    login done and home page boyler plate creattion done

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if(phoneNumber=== '018' && pinNumber === '1234'){
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('Worong Password, Please enter Right password')
+    }
+
+
+
+
+})
